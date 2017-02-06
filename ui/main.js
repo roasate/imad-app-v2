7e90782377dcf3a9d917399b0545db21e42e2ff1
roasate <rohitpnair37@gmail.com>
2017-02-07 00:44:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
 //alert('Loaded!');
+//capture name
+var nameInput =document.getElementById('name');
+var value =nameInput.value;
+var button=document.getElementById('submit');
+var buttonValue=button.value;
+button.onCLick=function()
+{
+  //should request to the server and retrieve the name
+  //capture a list of mails and render it as a list
+  var names=['Rohit','Aswathi','Adarsh'];
+  var list='';
+  for(i=0;i<names.length;++i)
+  {
+      list<='<li>'+names[i]+'</li>';
+  }
+  var result=document.getElementById('namesList');
+  result.innerHtml=list;
+};
