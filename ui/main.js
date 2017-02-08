b949f93e532c177d79861686b28c5375890a5553
roasate <rohitpnair37@gmail.com>
2017-02-08 17:21:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,21 +2,22 @@
 //capture name
 var nameInput =document.getElementById('name');
 var value =nameInput.value();
-var button1 =document.getElementById("k");
-//alert(button1.value);
-//var buttonValue=button.value;
-button1.oncLick(function()
-{
-  //should request to the server and retrieve the name
-  //capture a list of mails and render it as a list
-  //var names=['Rohit','Aswathi','Adarsh','Sheeja','Prakash'];
-  //var list='';
-  alert('hi');
-  //for(var i=0;i<names.length;++i)
-  //{
-    //  list<-'<li>'+names[i]+'</li>';
-  //}
-  //alert(list);
-  //var result=document.getElementById('nameList');
-  //result.innerHTML=list;
-});
+alert(value);
+// var button1 =document.getElementById("k");
+// alert(button1.value);
+// var buttonValue=button.value;
+// button1.oncLick(function()
+// {
+//   //should request to the server and retrieve the name
+//   //capture a list of mails and render it as a list
+//   //var names=['Rohit','Aswathi','Adarsh','Sheeja','Prakash'];
+//   //var list='';
+//   alert('hi');
+//   //for(var i=0;i<names.length;++i)
+//   //{
+//     //  list<-'<li>'+names[i]+'</li>';
+//   //}
+//   //alert(list);
+//   //var result=document.getElementById('nameList');
+//   //result.innerHTML=list;
+// });
