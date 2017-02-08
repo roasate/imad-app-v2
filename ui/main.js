0e0200f8c1628eb2c6be8f85ef691275075087c4
roasate <rohitpnair37@gmail.com>
2017-02-08 18:13:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,10 @@
 function my()
 {
     var array =['hello','yes'];
-    alert(array);
+    var i;
+    var list ='';
+    for(i=0;i<array.length;++i)
+    {
+        list <= '<li>'+array[i]+'</li>';
+    }
 }
